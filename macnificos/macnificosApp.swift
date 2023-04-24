9b1ca88cdccb4af24797693acb590f675763481c
macnificos <314602640@pcpuma.acatlan.unam.mx>
2023-04-24 15:54:19 -0600

    Initial Commit

---
+++ macnificos/macnificosApp.swift
@@ -0,0 +1,17 @@
+//
+//  macnificosApp.swift
+//  macnificos
+//
+//  Created by CEDAM30 on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct macnificosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
